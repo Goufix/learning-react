d1f7f022cb25a3bc4ab64c89ca43809be542e873
Goufix <alifer.17.11@gmail.com>
2019-06-16 18:51:17 -0300

    feat: PostController version without image upload

--- server/app/Controllers/Http/PostController.js
+++ server/app/Controllers/Http/PostController.js
@@ -1,12 +1,74 @@
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Post = use('App/Models/Post');
+/**
+ * Resourceful controller for interacting with posts
+ */
 class PostController {
-  async index({ request, response, view }) {}
+  /**
+   * Show a list of all posts.
+   * GET posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index() {
+    const data = await Post.all();
+    return data;
+  }
+  /**
+   * Create/save a new post.
+   * POST posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    const data = request.only([
+      'author',
+      'place',
+      'description',
+      'hashtags',
+      'likes'
+    ]);
+    const post = await Post.create(...data);
 
-  async store({ request, response }) {}
+    return post;
+  }
 
+  /**
+   * Display a single post.
+   * GET posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
   async show({ params, request, response, view }) {}
 
+  /**
+   * Update post details.
+   * PUT or PATCH posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
   async update({ params, request, response }) {}
 
+  /**
+   * Delete a post with id.
+   * DELETE posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
   async destroy({ params, request, response }) {}
 }
 

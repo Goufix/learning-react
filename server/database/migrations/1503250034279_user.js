66c9c5068b5f877269377ac40fdd089a501fa576
Goufix <alifer.17.11@gmail.com>
2019-06-16 18:26:01 -0300

    lint: lint

--- server/database/migrations/1503250034279_user.js
+++ server/database/migrations/1503250034279_user.js
@@ -1,25 +1,25 @@
-const Schema = use('Schema')
+const Schema = use('Schema');
 
 class UserSchema extends Schema {
   up() {
     this.create('users', (table) => {
-      table.increments()
+      table.increments();
       table
         .string('username', 80)
         .notNullable()
-        .unique()
+        .unique();
       table
         .string('email', 254)
         .notNullable()
-        .unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
-    })
+        .unique();
+      table.string('password', 60).notNullable();
+      table.timestamps();
+    });
   }
 
   down() {
-    this.drop('users')
+    this.drop('users');
   }
 }
 
-module.exports = UserSchema
+module.exports = UserSchema;

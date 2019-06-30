b6c85e1fbb06dfd047616d371a7311264218e3c6
Goufix <alifer.17.11@gmail.com>
2019-06-30 19:15:57 -0300

    WIP

--- client/web/src/screens/Feed/index.js
+++ client/web/src/screens/Feed/index.js
@@ -1,29 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import image from '../../1561000075401-image.jpg';
 import comment from '../../assets/comment.svg';
 import like from '../../assets/like.svg';
-import more from '../../assets/more.svg';
 import send from '../../assets/send.svg';
-import api from '../../services/api';
-import { Article, Description, Footer, FooterImg, Hashtags, Header, HeaderContent, Images, Img, Likes, More, Name, Place, PostImg, Section } from './syles';
+import api, { baseURL } from '../../services/api';
+import { Article, Description, Footer, FooterImg, Hashtags, Header, HeaderContent, Images, Img, Likes, More, Name, Place, Section } from './syles';
 
 export default function Feed() {
-  const [feed, setFeed] = useState('')
+  const [feed, setFeed] = useState([])
   useEffect(() => {
     async function getFeed() {
-      const response = await api.get('posts');
+      const response = await api.get('post');
 
       setFeed(response.data);
+      console.log(feed)
     }
     getFeed();
   }
   ) 
   return (
     <Section>
-      <Article>
+      {feed.map((value) => (
+        <Article>
+          <Header>
+            <HeaderContent>
+              <Name>{value.author}</Name>
+              <Place>{value.place}</Place>
+            </HeaderContent>
+            <More>
+              <Img src={`${baseURL}/images/${value.image}`} />
+            </More>
+          </Header>
+          <Footer>
+          <Images>
+            <FooterImg src={like} alt="" />
+            <FooterImg src={comment} alt="" />
+            <FooterImg src={send} alt="" />
+          </Images>
+
+          <Likes>{`${value.likes} Curtidas`}</Likes>
+          <Description>
+            {value.description}
+            <Hashtags>{value.hashtags}</Hashtags>
+          </Description>
+          </Footer>
+        </Article>
+      ))}
+      {/* <Article>
         <Header>
           <HeaderContent>
-            <Name> Alifer Quevedo </Name>
+            <Name> {feed.author} </Name>
             <Place> Piraquara/PR </Place>
           </HeaderContent>
           <More>
@@ -44,7 +69,7 @@ export default function Feed() {
             <Hashtags>#Massa #LearnReactJS</Hashtags>
           </Description>
         </Footer>
-      </Article>
+      </Article> */}
     </Section>
   );
 }
